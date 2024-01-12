abca8b793886ea3af9411f43194491e318c1c4f9
pbaranskaya <p.baranskaya@gmail.com>
2024-01-12 18:39:58 +0300

    12.01.2023

--- src/by/academy/homework4/Task4/IteratorApp.java
+++ src/by/academy/homework4/Task4/IteratorApp.java
@@ -1,9 +1,14 @@
 package by.academy.homework4.Task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IteratorApp {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4};
+        Integer[] array = {1, 6, 7, 4, 2};
+        MyIterator<Integer> myIterator = new MyIterator<>(array);
+
 
     }
 }
