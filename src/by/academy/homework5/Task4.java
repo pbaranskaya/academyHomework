a461930d823d1c0aa54f3b07b7b68810921149c6
pbaranskaya <p.baranskaya@gmail.com>
2024-01-20 17:17:53 +0300

    20.01.2023

---
+++ src/by/academy/homework5/Task4.java
@@ -0,0 +1,34 @@
+package by.academy.homework5;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Task4 {
+    public static void main(String[] args) {
+
+        int listSize = 10;
+
+        ArrayList<Integer> marks = new ArrayList<Integer>();
+
+        for (int i = 0; i < listSize; i++) {
+            int a = (int) (Math.random() * 10 + 1);
+            marks.add(i, a);
+        }
+
+        System.out.print("Оценки: " + marks + " ");
+        System.out.println();
+
+        ListIterator<Integer> listIterator = marks.listIterator();
+        while (listIterator.hasNext()) {
+            int max = 0;
+            for (Integer el : marks) {
+                el = listIterator.next();
+                if (el > max) {
+                    max = el;
+                }
+            }
+            System.out.println("Максимальная оценка: " + max);
+        }
+    }
+}
+
