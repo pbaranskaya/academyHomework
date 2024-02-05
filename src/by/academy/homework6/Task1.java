2d27acb0a00665f471bf255e292b37711be35961
pbaranskaya <p.baranskaya@gmail.com>
2024-02-05 14:33:31 +0300

    05.02.2024

---
+++ src/by/academy/homework6/Task1.java
@@ -0,0 +1,38 @@
+package by.academy.homework6;
+
+import java.io.*;
+
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+
+        String str;
+
+        File dirs = new File("src/files");
+
+        if (!dirs.exists()) {
+            dirs.mkdirs();
+        }
+
+        File file = new File(dirs, "homework6.txt");
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             FileWriter fw = new FileWriter(file);
+        ) {
+
+            System.out.println("Введите строку: ");
+            do {
+                str = br.readLine();
+                if (str.compareTo("stop") == 0)
+                    break;
+                str = str + "\n";
+                fw.write(str);
+            } while (str.compareTo("stop") != 0);
+            System.out.println("Запись завершена");
+        }
+    }
+}
+
